f5c2628368fc9975bff4bda8d2fe09c3cfc82a60
th-shivam <dreamyshivam01@gmail.com>
2026-01-31 22:32:39 +0530

    Updated app name to Chemical Equpiment Parameter Visualizer

--- web-frontend/src/App.js
+++ web-frontend/src/App.js
@@ -1,10 +1,12 @@
 import FileUpload from './FileUpload';
+import History from './History';
 
 function App() {
   return (
     <div className="App">
       <h1>Chemical Equipment Parameter Visualizer</h1>
       <FileUpload />
+      <History />
     </div>
   );
 }
