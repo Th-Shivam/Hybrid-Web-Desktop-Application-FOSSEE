e6b7a0bd59a8ea3c7efef26c4e32daa6639e2ef5
th-shivam <dreamyshivam01@gmail.com>
2026-01-31 10:16:35 +0530

    refactor: rename backend project to django_project

--- backend/django_project/settings.py
+++ backend/django_project/settings.py
@@ -51,7 +51,7 @@ MIDDLEWARE = [
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
 ]
 
-ROOT_URLCONF = "backend_conf.urls"
+ROOT_URLCONF = "django_project.urls"
 
 TEMPLATES = [
     {
@@ -69,7 +69,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = "backend_conf.wsgi.application"
+WSGI_APPLICATION = "django_project.wsgi.application"
 
 
 # Database
