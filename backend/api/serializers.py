9894a6fb8802ce3dbe08882c308dfa40c7534c8d
th-shivam <dreamyshivam01@gmail.com>
2026-01-31 16:16:30 +0530

    add serializer for upload history

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,12 @@
 from rest_framework import serializers
-from .models import Item
+from .models import Item, CSVHistory
 
 class ItemSerializer(serializers.ModelSerializer):
     class Meta:
         model = Item
+        fields = '__all__'
+
+class CSVHistorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = CSVHistory
         fields = '__all__'
